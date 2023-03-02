d2a1bf2e3886af850611736499165c01f20188e4
Ian Gomes <ianitalo56@hotmail.com>
2023-03-02 12:25:12 +0000

    fixed merge

--- FileReader.py
+++ FileReader.py
@@ -1,19 +1,32 @@
 import csv
+import Establishment
 
 class FileReader:
-# opening the CSV file
-    with open('establishments.csv', mode='r') as file:
-        # reading the CSV file
-        csvFile = csv.reader(file)
+    
+    @staticmethod
+    def getEstablishments():
+        # opening the CSV file
+        est = []
+        with open('establishments.csv', mode='r') as file:
+            # reading the CSV file
+            csvFile = csv.reader(file)
 
-        # displaying the contents of the CSV file
-        for lines in csvFile:
-            print(lines)
+            # displaying the contents of the CSV file
+            first = True
+            for lines in csvFile:
+                if(first):
+                    first = False 
+                    continue
+                est.append(Establishment.Establishment(lines[0],lines[1],lines[2],lines[3],lines[4],lines[5],lines[6],lines[7],lines[8],lines[9]))
+        return est
 
-    with open('distances.csv', mode='r') as file:
-        # reading the CSV file
-        csvFile = csv.reader(file)
 
-        # displaying the contents of the CSV file
-        for lines in csvFile:
-            print(lines)
+    @staticmethod
+    def getDistances():
+        with open('distances.csv', mode='r') as file:
+            # reading the CSV file
+            csvFile = csv.reader(file)
+
+            # displaying the contents of the CSV file
+            for lines in csvFile:
+                print(lines)
