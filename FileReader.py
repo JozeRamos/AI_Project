7bbfbd8ca7f6db0b889c714bf6f36e9acccb6545
Ian Gomes <ianitalo56@hotmail.com>
2023-03-02 11:47:32 +0000

    initial commit

---
+++ FileReader.py
@@ -0,0 +1,19 @@
+import csv
+
+class FileReader
+# opening the CSV file
+with open('establishments.csv', mode='r') as file:
+    # reading the CSV file
+    csvFile = csv.reader(file)
+
+    # displaying the contents of the CSV file
+    for lines in csvFile:
+        print(lines)
+
+with open('distances.csv', mode='r') as file:
+    # reading the CSV file
+    csvFile = csv.reader(file)
+
+    # displaying the contents of the CSV file
+    for lines in csvFile:
+        print(lines)
