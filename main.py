38eae2e90e26f38fa634fa33b623a08d3f99642b
JozeRamos <up202005460@up.pt>
2023-03-09 11:46:58 +0000

    fix greedy

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import math
+import sys
 from file_reader import FileReader
 from car import Car
 INF = 2 ** 24
@@ -30,9 +31,9 @@ if __name__ == '__main__':
     # best local = inspec duration + travel time + getWaitingTime()
     #for i in range(len(distances)):
 
-    for i in range(10):
+    for i in range(11):
         for car in cars:   
-            min_time = INF
+            min_time = sys.maxsize
             flag = False
             for estab in establishments:               
                 if estab.visited or (estab.id == car.place): continue
@@ -45,6 +46,7 @@ if __name__ == '__main__':
                  continue
             print(f'car with id {car.id} in place {car.place} going to {next_hop.id} with {min_time} second, or {round(min_time/3600,2)}h with fulltime {car.time/3600}')
             car.place = next_hop.id
+
             next_hop.visited = True
             car.time = min_time + car.time
             car.route.append(next_hop.id)
