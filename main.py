02f818b94d1f16427fa0f4747ad967e3d19df7e9
JozeRamos <up202005460@up.pt>
2023-03-23 12:13:50 +0000

    generic functions added

--- main.py
+++ main.py
@@ -25,7 +25,43 @@ def waitingTime(car_time, opening_hours):
         if opening_hours[i] == 1:
             return (i + next_day)*3600
     return INF
-        
+def randomSolution(cars, est, distances):
+    cities = list(range(len(est)))
+
+    for i in range(len(est)):        
+        for car in cars:
+            randomCity = cities[random.randint(0, len(cities) - 1)]
+            time = float(est[randomCity].inspec_duration) + float(distances[car.place][int(est[randomCity].id)]) + waitingTime(car.time + float(distances[car.place][int(est[randomCity].id)]), est[randomCity].opening_hours)
+            #print(time)
+            car.place = est[randomCity].id
+            est[randomCity].visited = True
+            car.time += time
+            car.route.append(est[randomCity].id)
+            cities.remove(randomCity)
+            if len(cities) == 0:
+                break
+        if len(cities) == 0:
+            break
+    # for car in cars:
+    #     print(car.route)
+    #     print(car.time/3600)        
+    return
+
+def routeTime(solution,est,distances):
+    routeTime = 0
+    for k in range(len(solution)-1):
+        a = solution[k]
+        i = solution[k+1]
+        inspect = float(est[i].inspec_duration)
+        #print(inspect)
+        dist = float(distances[a][int(est[i].id)])
+        #print(dist)
+        wait = waitingTime(routeTime + float(distances[a][int(est[i].id)]), est[i].opening_hours)
+        #print(wait)
+        routeTime += inspect + dist + wait
+        #print("-----------------")
+    return routeTime
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     establishments = FileReader.get_establishments()
