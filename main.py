356ddfe68d78d997de6f64133027346e0c7c2b2f
JozeRamos <up202005460@up.pt>
2023-03-23 12:10:25 +0000

    all ready to start

--- main.py
+++ main.py
@@ -33,41 +33,4 @@ if __name__ == '__main__':
 
     cars = [Car(x) for x in range(100)]
     
-    # best local = inspec duration + travel time + getWaitingTime()
-    #for i in range(len(distances)):
 
-    for i in range(11):
-        for car in cars:   
-            min_time = sys.maxsize
-            flag = False
-            for estab in establishments:               
-                if estab.visited or (estab.id == car.place): continue
-                time = float(estab.inspec_duration) + float(distances[car.place][int(estab.id)]) + waitingTime(car.time + float(distances[car.place][int(estab.id)]), estab.opening_hours)
-                if(time < min_time):
-                     min_time = round(time,2)
-                     next_hop = estab
-                     flag = True
-            if not flag:
-                 continue
-            print(f'car with id {car.id} in place {car.place} going to {next_hop.id} with {min_time} second, or {round(min_time/3600,2)}h with fulltime {car.time/3600}')
-            car.place = next_hop.id
-
-            next_hop.visited = True
-            car.time = min_time + car.time
-            car.route.append(next_hop.id)
-    
-    print(f'total estabs: {len(establishments)}')
-    print(f'visited estabs: {len([x for x in establishments if x.visited])}')
-    print(f'not visited estabs: {len([x for x in establishments if not x.visited])}')
-  #  for estab in establishments:
- #      if not estab.visited:
-#             print(f'estab {estab.id} is not visited!')
-
-
-        
-        
-
-
-
-#inspec duration + travel time + getWaitingTime()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
